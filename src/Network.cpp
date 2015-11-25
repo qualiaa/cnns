56a082fc0ad34a9424f39aafac0405605a6e806c
Jamie Bayne <jamiebayne@hotmail.com>
2015-11-25 20:17:43 +0000

    Make layer indexing slightly more sensible

--- src/Network.cpp
+++ src/Network.cpp
@@ -32,29 +32,33 @@ T randf(T a = 0.0, T b = 1.0)
 
 Network::Network(std::vector<int> const& sizes)
     : layers{sizes.size()}
+    , weights(layers)
+    , biases(layers)
 {
-    for (unsigned l = 0; l+1 < layers; ++l) {
-        const int n_in = sizes[l], n_out = sizes[l+1];
+    for (unsigned l = 1; l < layers; ++l) {
+        const int n_in = sizes[l-1], n_out = sizes[l];
 
-        weights.emplace_back(n_out, n_in, arma::fill::randu);
-        biases.emplace_back(n_out, arma::fill::randu);
+        weights[l] = Mat(n_out, n_in, arma::fill::randu);
+        std::cout << l << ": " << n_out << std::endl;
+        biases[l] = Vec(n_out, arma::fill::randu);
     }
 }
 
 std::tuple<VecList,VecList> Network::feed_forward(Vec const& x) const
 {
-    VecList activations{x};
-    VecList zs;
+    VecList activations(layers, x);
+    VecList weighted_inputs(layers);
 
-    for (unsigned l = 0; l+1 < layers; ++l) {
+    for (unsigned l = 1; l < layers; ++l) {
         auto const& W = weights[l];
         auto const& b = biases[l];
-        auto const& x = activations.back();
+        auto const& x = activations[l-1];
         auto const z = W * x + b;
-        zs.push_back(z);
-        activations.push_back(activation(z));
+
+        weighted_inputs[l] = z;
+        activations[l] = activation(z);
     }
-    return std::tuple<VecList,VecList>(activations, zs);
+    return std::tuple<VecList,VecList>(activations, weighted_inputs);
 }
 
 Vec Network::classify(Vec const& x) const
@@ -77,7 +81,7 @@ void Network::train(Mat const& data, Mat const& classes, double learning_rate)
                               << classes.n_rows << " desired outputs\n";
         throw std::invalid_argument(stream.str());
     }
-    if (data.n_cols != weights.front().n_cols) {
+    if (data.n_cols != weights[1].n_cols) {
         stream << "Instance data must fill input layer:\n"
                << "Provided " << data.n_cols << " data, have "
                               << weights.front().n_cols << "input nodes\n";
@@ -92,11 +96,12 @@ void Network::train(Mat const& data, Mat const& classes, double learning_rate)
 
     learning_rate /= n_instances;
 
-    MatList delta_weights;
-    VecList delta_biases;
-    for (unsigned l = 0; l+1 < layers; ++l) {
-        delta_weights.emplace_back(size(weights[l]), arma::fill::zeros);
-        delta_biases.emplace_back(size(biases[l]), arma::fill::zeros);
+    MatList delta_weights(layers);
+    VecList delta_biases(layers);
+
+    for (unsigned l = 1; l < layers; ++l) {
+        delta_weights[l] = Mat(size(weights[l]), arma::fill::zeros);
+        delta_biases[l] = Vec(size(biases[l]), arma::fill::zeros);
     }
 
     for (unsigned i = 0; i < n_instances; ++i) {
@@ -106,26 +111,27 @@ void Network::train(Mat const& data, Mat const& classes, double learning_rate)
         VecList activations, zs;
         std::tie(activations, zs) = feed_forward(x);
 
-        const Vec delta_cost = dCost(y, activations.back());
+        const Vec dCda = dCost(y, activations.back());
         
-        Vec running_derivative = delta_cost % dActivation(zs.back()); // delta
+        Vec running_derivative = dCda % dActivation(zs.back()); // delta
 
         // back-propagation + gradient descent
-        for (int l = layers - 1; l > 0; --l) {
+        for (int l = layers - 1; l > 1; --l) {
             // update deltas with dC wrt weights and biases in l
-            delta_weights[l-1] += arma::dot(running_derivative, activations[l]);
-            delta_biases[l-1] += running_derivative;
+            delta_weights[l] += arma::dot(running_derivative, activations[l]);
+            std::cout << delta_weights[l];
+            delta_biases[l] += running_derivative;
 
             // update delta
-            if (l > 1) {
-                running_derivative = (weights[l-1].t() * running_derivative) % dActivation(zs[l-2]);
-            }
+            running_derivative = (weights[l].t() * running_derivative) % dActivation(zs[l-1]);
 
             // modify weights according to gradient descent
-            weights[l-1] -= learning_rate * delta_weights[l-1];
-            biases[l-1]  -= learning_rate * delta_biases[l-1];
+            weights[l] -= learning_rate * delta_weights[l];
+            biases[l]  -= learning_rate * delta_biases[l];
 
         }
+        std::cout << "Wanted: " << y;
+        std::cout << "Found: " << activations.back();
     }
 }
 }
